0b03a2789703884afd3064967b79466541005a6c
Jaber Alsalamah <SWE.Jaber@gmail.com>
2025-02-15 12:43:12 +0300

    implement flying, change plane position

--- plane/plane.gd
+++ plane/plane.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 const GRAVITY: float = 600.0
+const POWER: float = -200.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,8 +11,16 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	
+	
 #	This will have an acceleration effect
 	velocity.y += GRAVITY * delta
 	
+	fly()
 #	This already multiplies the GRAVITY constant by delta
 	move_and_slide()
+
+
+func fly() -> void:
+	if Input.is_action_just_pressed("fly"):
+		velocity.y = POWER
+	
