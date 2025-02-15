66ba4bfc9f3969b02a4c10399a538189ee2d408f
Jaber Alsalamah <SWE.Jaber@gmail.com>
2025-02-15 13:49:42 +0300

    add ceiling and floor to game scene

--- plane/plane.gd
+++ plane/plane.gd
@@ -1,7 +1,10 @@
 extends CharacterBody2D
 
-const GRAVITY: float = 600.0
-const POWER: float = -200.0
+const GRAVITY: float = 1000.0
+const POWER: float = -350.0
+
+@onready var anim_sprite: AnimatedSprite2D = $AnimSprite
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,16 +14,23 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	
-	
-#	This will have an acceleration effect
+	#	This will have an acceleration effect
 	velocity.y += GRAVITY * delta
 	
 	fly()
 #	This already multiplies the GRAVITY constant by delta
 	move_and_slide()
+	
+	
+	if is_on_floor():
+		die()
 
 
 func fly() -> void:
 	if Input.is_action_just_pressed("fly"):
 		velocity.y = POWER
 	
+
+func die() -> void:
+	anim_sprite.stop()
+	set_physics_process(false)
