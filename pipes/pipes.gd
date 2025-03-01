c0af685aabf3f8f516d138122d81075931285870
Jaber Alsalamah <SWE.Jaber@gmail.com>
2025-03-02 00:43:44 +0300

    implement laser collision

--- pipes/pipes.gd
+++ pipes/pipes.gd
@@ -3,6 +3,7 @@ extends Node2D
 class_name Pipes
 
 const OFF_SCREEN: float = -500
+@onready var score_sound: AudioStreamPlayer2D = $ScoreSound
 
 func _ready() -> void:
 	SignalManager.on_plane_died.connect(on_plane_died)
@@ -26,3 +27,8 @@ func _on_screen_exited() -> void:
 func _on_pipe_body_entered(body: Node2D) -> void:	
 	if body is Tappy:
 		body.die()
+
+
+func _on_laser_body_entered(body: Node2D) -> void:
+	if body is Tappy:
+		score_sound.play()
