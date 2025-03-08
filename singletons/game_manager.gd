9f98d477e7b75bfb99b4cfc1babe23bc005ea21b
Jaber Alsalamah <SWE.Jaber@gmail.com>
2025-03-08 22:46:24 +0300

    implement scene navigation

--- singletons/game_manager.gd
+++ singletons/game_manager.gd
@@ -1,4 +1,12 @@
 extends Node
 
+
+const MAIN_MENU: PackedScene = preload("res://scenes/main_menu.tscn")
+const GAME: PackedScene = preload("res://scenes/game.tscn")
+
 const SCROLL_SPEED: float = 120
 const PLANE_GROUP: String = "Plane"
+
+
+func load_game_scene() -> void: 
+	get_tree().change_scene_to_packed(GAME)
